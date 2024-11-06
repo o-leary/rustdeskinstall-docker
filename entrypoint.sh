b5974b95aefdaf58c40e06cb55705f62e4658a25
Sean O'Leary <sean@o-leary.co.nz>
2024-11-07 01:37:43 +1300

    Update entrypoint.sh
    Move install.sh call into entryfile so we can use existing volumes for configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 HTTP_USER="$(< /run/secrets/http_user)"
 HTTP_PASS="$(< /run/secrets/http_pass)"
 
+if [ ! -f /opt/rustdesk/hbbs ]; then
+        cd /app
+        ./install.sh --resolveip --install-http
+fi
+
 cd /opt/rustdesk
 ./hbbs &
 ./hbbr &
